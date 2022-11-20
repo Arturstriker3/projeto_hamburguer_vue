dc6df5faf6975a073c23b05c839250f9c0643bd0
Artur Daniel <59231364+Arturstriker3@users.noreply.github.com>
2022-11-20 12:46:14 -0300

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="nav">
+        <router-link id="logo-url" to="/">
+            <img id="logo" :src="logo" :alt="alt">
+        </router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/Pedidos" v-if="esta_logado">Pedidos</router-link>
+        <router-link to="/Acesso">Acesso</router-link>
+        
+    </div>
+    
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+    export default {
+        
+        name: "Navbar",
+        props: ["logo","alt",],
+
+        computed: {
+                ...mapState ({
+                    esta_logado: state => state.esta_logado
+                })
+        },
+
+        methods: {
+
+        },
+
+        components: {
+            
+        },
+
+        
+    }   
+    
+    
+</script>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    #nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+
+    }
+
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a {
+        color: #fbca03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover {
+        color: #FFF;
+
+
+    }
+</style>
